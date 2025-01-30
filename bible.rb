81cedab91b4d0e2870b39eefef6c4c67d395a0ed
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-30 15:43:40 +0000

    rough ai base for bible passages

--- bible.rb
+++ bible.rb
@@ -1,6 +1,7 @@
 require "http"
 require "json"
 require "dotenv/load"
+require "openai"
 
 
 ESV_API_KEY = ENV.fetch("ESV_API_KEY")
@@ -8,9 +9,9 @@ API_URL = "https://api.esv.org/v3/passage/text/"
 
 
 
-puts "Please search a bible passage"
-passage = gets.chomp
-# passage = "Jonah 1-2"
+# puts "Please search a bible passage"
+# passage = gets.chomp
+passage = "Jonah 1-2"
 
 
 def get_esv_text(passage)
@@ -37,4 +38,54 @@ def get_esv_text(passage)
   passages.any? ? passages[0].strip : "Error: Passage not found"
 end
 
-puts get_esv_text(passage)
+
+
+########################################
+
+# client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+# message_list = [
+#   {
+#     "role" => "system",
+#     "content" => "You are a helpful assistant. When asked about Bible passages, you will respond strictly with a list of the passage references,\
+#       each separated by a comma with no whitespace. If you have no Bible passage references that match the question, respond with "
+#   }
+# ]
+
+
+def ai_search_passage()
+  puts "Hello! How can I help you today?"
+  puts "-"*50
+
+  client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+  message_list = [
+    {
+      "role" => "system",
+      "content" => "You are a helpful assistant. When asked about Bible passages, you will respond strictly with a list of the passage references,\
+        each separated by a comma with no whitespace. If you have no Bible passage references that match the question, respond with "
+    }
+  ]
+
+  query = gets.chomp
+  # query = "How do I compute 5*5?"
+
+  # Call the API to get the next message from GPT
+  api_response = client.chat(
+    parameters: {
+      model: "gpt-3.5-turbo",
+      messages: message_list
+    }
+  )
+
+  # pp api_response
+
+  ai_message = api_response["choices"][0].fetch("message").fetch("content")
+  puts ai_message
+  puts "-"*50
+end
+
+ai_search_passage()
+
+
+##########################################################
+
+# puts get_esv_text(passage)
